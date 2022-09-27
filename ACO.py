74625618af1e8b07da63c986ade0dd0d398c0d61
chingxuen <chingxuen@gmail.com>
2022-09-27 17:28:40 +0800

    Update ACO.py

--- ACO.py
+++ ACO.py
@@ -19,6 +19,7 @@ class Road:
         self.connected_shops = connected_shops
         self.cost = cost
         self.pheromone = pheromone
+       
         
     def set_pheromone(self, pheromone):
         self.pheromone = pheromone
@@ -27,12 +28,15 @@ class Road:
         # update the pheromone of the road
         self.pheromone = (1-rho) * self.pheromone
         
-    def deposit_pheromone(self, ants):
+    def deposit_pheromone(self, ants, intermediate_shops):
         # 1. search for ants that uses the road
         for ant in ants:
             if self in ant.path:
                 # 2. deposit pheromone using the inveresely proportionate relationship between path length and deposited pheromone
-                self.pheromone += 1 / ant.get_path_length() 
+                if all(shop in ant.shops for shop in intermediate_shops):
+                    self.pheromone += 1 / (ant.get_path_length() ** 1)
+                else:
+                    self.pheromone += 1 / (ant.get_path_length() ** 1000)
                 
 class Ant:
     def __init__(self):
@@ -40,84 +44,97 @@ class Ant:
         self.path = []       # Roads the ant uses, in sequence 
         
     # Find path from origin to destination
-    def get_path(self, origin, destination, alpha, intermediate_shops):
+    def get_path(self, origin, destination, alpha, intermediate_shops, atriums):
         # 1. append origin to the self.shops
         self.shops.append(origin)
-        # 2. if the last shop is not destination, search for the next shop to go
+        # 2. if the last shop is not destination and shops do not contain all intermediate shops, search for the next shop to go
         while (self.shops[-1] != destination) or (all(shop in self.shops for shop in intermediate_shops) == False):
         #for i in range(20):
             current_shop = self.shops[-1]
             current_roads = []
-            probabilities = []
-            pheromone_sum = 0
-            selectedProbability = random.random()
-            currentCumulatedProbability = 0
             
-            # Remove road that was just used 
-            for i in range(len(current_shop.roads)):
-                if len(self.path) != 0:
-                    if (current_shop.roads[i] != self.path[-1]) or (len(current_shop.roads) == 1):     # if road was not just used, or if it is the only possible road 
-                        current_roads.append(current_shop.roads[i])
-                else:
-                    current_roads.append(current_shop.roads[i])
+            # Remove road that was just used - unless it was an atrium or there is no other path
+            current_roads = self.remove_used_roads(current_shop, current_roads)
                 
-            # Get total pheromone of roads connected to a shop
-            for i in range(len(current_roads)):
-                pheromone_sum += (current_roads[i].pheromone ** alpha)
-
-            # Calculate probabilities of each road connected to the shop 
-            for i in range(len(current_roads)):
-                p = ((current_roads[i].pheromone ** alpha) / pheromone_sum)
-                probabilities.append(p)
-
-            # Select road and append to path list 
-            for choice, prob in zip(current_roads, probabilities):
-                currentCumulatedProbability += prob
-                if selectedProbability < currentCumulatedProbability:
-                    road_chosen = choice
-                    self.path.append(road_chosen)
-                    break
+            # Select road based on roulette wheel method 
+            road_chosen = self.select_road(current_roads)
 
             #print(str(self.path[-1].connected_shops[0].name) + ' ' + str(self.path[-1].connected_shops[1].name))
             
-            # Add Shop to the list of shops based on road
-            if (road_chosen.connected_shops[0] == self.shops[-1]):
-                self.shops.append(road_chosen.connected_shops[1])
-            else: 
-                self.shops.append(road_chosen.connected_shops[0])
+            # Add Shop to the list of shops based on road chosen
+            self.add_shop(road_chosen)
                 
         # 3. after getting to the destination, remove the loop within the path, i.e. if there are repeated shops in self.shops, remove the shops and the roads in between the repetition
-        self.remove_loopy_paths()
-            
-        print()
+        self.remove_loopy_paths(intermediate_shops)
+        
         for i in range(len(self.shops)):
             if (i+1 < len(self.shops)):
                 print(self.shops[i].name, end=" -> ")
             else:
                 print(self.shops[i].name)
+        print()
         
     # Removes Loopy Paths in shops and path lists
-    def remove_loopy_paths(self):
-        while (len(set(self.shops)) != len(self.shops)):   # Repeat process until shops list only contain unique shops
-            temp = []
-            n = len(self.shops)
-            
-            for i in range(n):         # used to find the index of similar pairs 
-                for j in range(n):
-                    if (self.shops[i] == self.shops[j]) and (i < j):
-                        if (len(temp) > 0):            # if a duplicate has already been found
-                            if (j > temp[-1]):         # if another duplicate is found after the first one 
-                                temp = []
-                                temp.extend([i,j])     # extend temp list with index of duplicates
-                        else:
-                            temp.extend([i,j])
+    def remove_loopy_paths(self, intermediate_shops):  
+        # 1.0 - remove all in between if no intermediate shop in between
+        # indexes = []
+        # n = len(self.shops)
+        
+        # for i in range(n):
+        #     for j in range(n):       # if found duplicate, and there is no intermediate shop in between them
+        #         if (self.shops[i]) == self.shops[j] and (i < j) and (any(shop in self.shops[i:j] for shop in intermediate_shops) == False) and all(i>=sl[1] for sl in indexes):
+        #             indexes.append([i,j])
+        #             break
                 
-                if (len(temp) > 0):     # if duplicate found, break out of for loop and address it first
+        # print(indexes)
+        # for w in range(len(indexes)):
+        #       del self.shops[indexes[len(indexes)-1-w][0]: indexes[len(indexes)-1-w][1]]
+        #       del self.path[indexes[len(indexes)-1-w][0]: indexes[len(indexes)-1-w][1]]
+    
+        # # 2.0 - remove all in between if intermediate shop duplicate
+        # indexes = []
+        # n = len(self.shops)
+        
+        # for i in range(n):
+        #     for j in range(n):       # if intermediate shop duplicate, remove all in between
+        #         if (self.shops[i]) == self.shops[j] and (i < j) and (self.shops[i] in intermediate_shops) and all(i>=sl[1] for sl in indexes):
+        #             indexes.append([i,j])
+        #             break
+                
+        # print(indexes)
+        # for w in range(len(indexes)):
+        #       del self.shops[indexes[len(indexes)-1-w][0]: indexes[len(indexes)-1-w][1]]
+        #       del self.path[indexes[len(indexes)-1-w][0]: indexes[len(indexes)-1-w][1]]
+        
+        # # 3.0 - remove all in between unless there is atrium 
+        indexes = []
+        n = len(self.shops)
+        
+        for i in range(n):
+            for j in range(n):       # if duplicate, remove all in between unless there is atrium in between
+                if (self.shops[i]) == self.shops[j] and (i < j) and all(i>=sl[1] for sl in indexes) and (any(shop in self.shops[i:j] for shop in atriums) == False):
+                    indexes.append([i,j])
                     break
-            
-            #print(temp)
-            del self.shops[temp[0]: temp[1]]      # remove loopy path
-            del self.path[temp[0]: temp[1]]
+                
+        print(indexes)
+        for w in range(len(indexes)):
+              del self.shops[indexes[len(indexes)-1-w][0]: indexes[len(indexes)-1-w][1]]
+              del self.path[indexes[len(indexes)-1-w][0]: indexes[len(indexes)-1-w][1]]
+        
+        # 4.0 - remove all duplicates
+        # indexes = []
+        # n = len(self.shops)
+        
+        # for i in range(n):
+        #     for j in range(n):       # if intermediate shop duplicate, remove all in between
+        #         if (self.shops[i]) == self.shops[j] and (i < j) and all(i>=sl[1] for sl in indexes):
+        #             indexes.append([i,j])
+        #             break
+                
+        # print(indexes)
+        # for w in range(len(indexes)):
+        #       del self.shops[indexes[len(indexes)-1-w][0]: indexes[len(indexes)-1-w][1]]
+        #       del self.path[indexes[len(indexes)-1-w][0]: indexes[len(indexes)-1-w][1]]
                 
     # Calculate path length of each ant 
     def get_path_length(self):
@@ -131,6 +148,49 @@ class Ant:
     def reset(self):
         self.path = []
         self.shops = []
+        
+    # Remove road that was just used - unless it was an atrium or there is no other path
+    def remove_used_roads(self, current_shop, current_roads):
+        for i in range(len(current_shop.roads)):
+            if len(self.path) != 0:
+                # if road was not just used, or if it is the only possible road, or if previous shop was atrium 
+                if (current_shop.roads[i] != self.path[-1]) or (len(current_shop.roads) == 1) or (any(atrium == self.shops[-2] for atrium in atriums)):     
+                    current_roads.append(current_shop.roads[i])
+            else:
+                current_roads.append(current_shop.roads[i])
+        return current_roads
+           
+    def select_road(self, current_roads):
+        pheromone_sum = 0
+        probabilities = []
+        selectedProbability = random.random()
+        currentCumulatedProbability = 0
+        
+        # Get total pheromone of roads connected to a shop
+        for i in range(len(current_roads)):
+            pheromone_sum += (current_roads[i].pheromone ** alpha)
+    
+        # Calculate probabilities of each road connected to the shop 
+        for i in range(len(current_roads)):
+            p = ((current_roads[i].pheromone ** alpha) / pheromone_sum)
+            probabilities.append(p)
+    
+        # Select road and append to path list 
+        for choice, prob in zip(current_roads, probabilities):
+            currentCumulatedProbability += prob
+            if selectedProbability < currentCumulatedProbability:
+                road_chosen = choice
+                self.path.append(road_chosen)
+                break
+            
+        return road_chosen
+            
+    # Add Shop to the list of shops based on road
+    def add_shop(self, road_chosen):
+        if (road_chosen.connected_shops[0] == self.shops[-1]):
+            self.shops.append(road_chosen.connected_shops[1])
+        else: 
+            self.shops.append(road_chosen.connected_shops[0])
        
 # Calculate the percentage of the most dominant path 
 def get_percentage_of_dominant_path(ants):
@@ -183,72 +243,72 @@ if __name__ == "__main__":
     location_list = [ # [x,y,name]
       [12, 0, 'Main Entrance'],
       [7, 1, "Ben's Cafe"],
-      [12, 4, "South Atrium 1"],
+      [12, 4, "SA 1"],
       [17, 4, "Papa Cheah Pharmacy"],
       [6, 7, "Uncle Marcus"],
       [12, 10, "Ming's Iceland"],
       [22, 10, "Toilet 1"],
-      [4, 13, 'West Escalator 1'],
-      [7, 13, 'West Atrium 1'],
-      [18, 13, 'East Atrium 1'],
-      [20, 13, 'East Escalator 1'],
+      [4, 13, 'WE 1'],
+      [7, 13, 'WA 1'],
+      [18, 13, 'EA 1'],
+      [20, 13, 'EE 1'],
       [6, 19, "Ching's Fashion"],
-      [12, 21, "North Atrium 1"],
+      [12, 21, "NA 1"],
       [18, 21, "Richardson"],
       [12, 24, "Exit"],
       # Level 2
       [6, 5, "Manesh Banana Leaf"],
       [21, 4, "Lucas Fun Time"],
-      [12, 7, "South Atrium 2"],
+      [12, 7, "SA 2"],
       [2, 10, "Toilet 2"],
-      [4, 13, "West Escalator 2"],
-      [6, 13, "West Atrium 2"],
-      [18, 13, "East Atrium 2"],
-      [20, 13, "East Escalator 2"],
+      [4, 13, "WE 2"],
+      [6, 13, "WA 2"],
+      [18, 13, "EA 2"],
+      [20, 13, "EE 2"],
       [12, 15, "Alex Swag Wear"],
-      [12, 17, "North Atrium 2"],
+      [12, 17, "NA 2"],
       [3, 19, "Chee's Electronics"],
       [15, 20, "Elizabeth's Hair Care"]
     ]
     
     step_cost = [
-      ['Main Entrance', "Ben's Cafe", 30], ['Main Entrance', "South Atrium 1", 35],
+      ['Main Entrance', "Ben's Cafe", 30], ['Main Entrance', "SA 1", 35],
       ['Main Entrance', "Papa Cheah Pharmacy", 45], ["Ben's Cafe", "Papa Cheah Pharmacy", 65],
-      ["Ben's Cafe", "South Atrium 1", 55], ["Papa Cheah Pharmacy", "South Atrium 1", 40],
+      ["Ben's Cafe", "SA 1", 55], ["Papa Cheah Pharmacy", "SA 1", 40],
       
-      ["South Atrium 1", "Uncle Marcus", 30], ["South Atrium 1", "Ming's Iceland", 15],
-      ["South Atrium 1", "Toilet 1", 65], ["South Atrium 1", 'West Atrium 1', 55],
-      ["South Atrium 1", 'East Atrium 1', 60], ["Uncle Marcus", "West Atrium 1", 45],
-      ["East Atrium 1", "Toilet 1", 35],
+      ["SA 1", "Uncle Marcus", 30], ["SA 1", "Ming's Iceland", 15],
+      ["SA 1", "Toilet 1", 65], ["SA 1", 'WA 1', 55],
+      ["SA 1", 'EA 1', 60], ["Uncle Marcus", "WA 1", 45],
+      ["EA 1", "Toilet 1", 35],
       
-      ["West Atrium 1", "West Escalator 1", 15], ["East Atrium 1", "East Escalator 1", 10],
+      ["WA 1", "WE 1", 15], ["EA 1", "EE 1", 10],
       
-      ["West Atrium 1", "Ching's Fashion", 45], ["West Atrium 1", "North Atrium 1", 55],
-      ["Ching's Fashion", "North Atrium 1", 30], ["North Atrium 1", "Exit", 25],
-      ["North Atrium 1", "Richardson", 40], ["Richardson", "Exit", 45],
-      ["North Atrium 1", "East Atrium 1", 60],
+      ["WA 1", "Ching's Fashion", 45], ["WA 1", "NA 1", 55],
+      ["Ching's Fashion", "NA 1", 30], ["NA 1", "Exit", 25],
+      ["NA 1", "Richardson", 40], ["Richardson", "Exit", 45],
+      ["NA 1", "EA 1", 60],
       
       # Connection ----------------------------
-      ["West Escalator 1", "West Escalator 2", 5], ["East Escalator 1", "East Escalator 2", 5], 
+      ["WE 1", "WE 2", 5], ["EE 1", "EE 2", 5], 
       
-      # Level 2 - Currently not connected to lvl 1
-      ["West Escalator 2", "West Atrium 2", 10], ["East Escalator 2", "East Atrium 2", 10],
+      # Level 2 
+      ["WE 2", "WA 2", 10], ["EE 2", "EA 2", 10],
       
-      ["Manesh Banana Leaf", "South Atrium 2", 40], ["Manesh Banana Leaf", "Toilet 2", 45],
-      ["Manesh Banana Leaf", "West Atrium 2", 40], ["Toilet 2", "West Atrium 2", 35],
-      ["Toilet 2", "South Atrium 2", 65], ["South Atrium 2", "West Atrium 2", 60],
+      ["Manesh Banana Leaf", "SA 2", 40], ["Manesh Banana Leaf", "Toilet 2", 45],
+      ["Manesh Banana Leaf", "WA 2", 40], ["Toilet 2", "WA 2", 35],
+      ["Toilet 2", "SA 2", 65], ["SA 2", "WA 2", 60],
       
-      ["South Atrium 2", "East Atrium 2", 60], ["South Atrium 2", "Lucas Fun Time", 60],
-      ["Lucas Fun Time", "East Atrium 2", 60],
+      ["SA 2", "EA 2", 60], ["SA 2", "Lucas Fun Time", 60],
+      ["Lucas Fun Time", "EA 2", 60],
       
-      ["East Atrium 2", "North Atrium 2", 50],
+      ["EA 2", "NA 2", 50],
       
-      ["North Atrium 2", "Alex Swag Wear", 10],
+      ["NA 2", "Alex Swag Wear", 10],
       
-      ["North Atrium 2", "Elizabeth's Hair Care", 30],
+      ["NA 2", "Elizabeth's Hair Care", 30],
       
-      ["North Atrium 2", "West Atrium 2", 50], ["North Atrium 2", "Chee's Electronics", 55],
-      ["West Atrium 2", "Chee's Electronics", 45]
+      ["NA 2", "WA 2", 50], ["NA 2", "Chee's Electronics", 55],
+      ["WA 2", "Chee's Electronics", 45]
     ]
     
     shops = {} # Create dictionary
@@ -266,12 +326,14 @@ if __name__ == "__main__":
     # Define origin and destination shops 
     origin = shops["Main Entrance"]
     destination = shops["Toilet 1"]
-    intermediate_shops = [shops["North Atrium 1"]]
+    #intermediate_shops = [shops["Ben's Cafe"], shops["Papa Cheah Pharmacy"], shops["Uncle Marcus"], shops["Ching's Fashion"]]
+    intermediate_shops = [shops["Ben's Cafe"], shops["Uncle Marcus"], shops["Papa Cheah Pharmacy"]]
+    atriums = [shops["SA 1"], shops["WA 1"], shops["EA 1"], shops["NA 1"]]
     
     # Parameters Initilization
     n_ant = 10
     alpha = 1
-    rho = 0.1
+    rho = 0.3    # higher rho, better result
     final_paths = []
     solution = []
     # 1.0 Pheromone Initialization
@@ -294,7 +356,7 @@ if __name__ == "__main__":
             # reset the path of the ant
             ant.reset()
             # identify the path of the ant
-            ant.get_path(origin, destination, alpha, intermediate_shops)
+            ant.get_path(origin, destination, alpha, intermediate_shops, atriums)
         # calculate percentage of dominant paths
         percentage_of_dominant_path = get_percentage_of_dominant_path(ants)
         # loop through all roads
@@ -302,7 +364,7 @@ if __name__ == "__main__":
             # evaporate the pheromone on the road
             road.evaporate_pheromone(rho)
             # deposit the pheromone
-            road.deposit_pheromone(ants)
+            road.deposit_pheromone(ants, intermediate_shops)
         # increase iteration count
         iteration += 1
       
